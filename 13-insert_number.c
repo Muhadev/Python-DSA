10c8a767874cd27e35e40851d122ceb87a95e193
Muhadev <muhammedfayemi@gmail.com>
2024-01-02 22:04:02 +0000

    linked_lists.c is added

---
+++ 13-insert_number.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new_node = malloc(sizeof(listint_t));
+	listint_t *current = *head;
+	listint_t *prev = NULL;
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	while (current != NULL && current->n < number)
+	{
+		prev = current;
+		current = current->next;
+	}
+	if (prev == NULL)
+	{
+		new_node->next = *head;
+		*head = new_node;
+	}
+	else
+	{
+		prev->next = new_node;
+		new_node->next = current;
+	}
+	return (new_node);
+}
